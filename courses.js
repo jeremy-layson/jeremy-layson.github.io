81516eef52df6531da89315cc704a3c70dd90229
Jeremy Layson <jeremy.b.layson@gmail.com>
2023-01-24 08:06:09 +0800

    Add Masterclass

--- courses.js
+++ courses.js
@@ -46,5 +46,773 @@ let courses = [
       "mirrors": []
     },
     "tags": []
-  }
+  },
+  {
+    "title": "Aaron Franklin Teaches Texas-Style BBQ",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Aaron Sorkin Teaches Screenwriting",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Alice Waters Teaches The Art of Home Cooking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Alicia Keys Teaches Songwriting and Producing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Anna Wintour Teaches Creativity and Leadership",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Annie Leibovitz Teaches Photography",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Armin van Buuren Teaches Dance Music",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Billy Collins Teaches Reading and Writing Poetry",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Bob Iger Teaches Business Strategy and Leadership",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Bob Woodward Teaches Investigative Journalism",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Bobbi Brown Teaches Makeup and Beauty",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Brandon McMillan Teaches Dog Training",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Carlos Santana Teaches the Art and Soul of Guitar",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Chris Hadfield Teaches Space Exploration",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Chris Voss Teaches The Art of Negotiation",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Christina Aguilera Teaches Singing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Dan Brown Teaches Writing Thrillers",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Daniel Negreanu Teaches Poker",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Daniel Pink Teaches Sales and Persuasion",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Danny Elfman Teaches Music for Film",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "David Axelrod and Karl Rove Teach Campaign Strategy and Messaging",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "David Baldacci Teaches Mystery and Thriller Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "David Lynch Teaches Creativity and Film",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "David Mamet Teaches Dramatic Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "David Sedaris Teaches Storytelling and Humor",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Deadmau5 Teaches Electronic Music Production",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Diane von Furstenberg Teaches Building a Fashion Brand",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Dominique Ansel Teaches French Pastry Fundamentals",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Doris Kearns Goodwin Teaches U.S. Presidential History and Leadership",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Dr. Jane Goodall Teaches Conservation",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Emily Morse Teaches Sex and Communication",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Frank Gehry Teaches Design and Architecture",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Gabriela Cámara Teaches Mexican Cooking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Garry Kasparov Teaches Chess",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Gordon Ramsay Teaches Cooking I",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Gordon Ramsay Teaches Cooking II Restaurant Recipes at Home",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Hans Zimmer Teaches Film Scoring",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Helen Mirren Teaches Acting",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Herbie Hancock Teaches Jazz",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Howard Schultz Business Leadership",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Itzhak Perlman Teaches Violin",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Jake Shimabukuro Teaches Ukulele",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "James Patterson Teaches Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "James Suckling Teaches Wine Appreciation",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Jeff Goodby & Rich Silverstein Teach Advertising and Creativity",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Jimmy Chin Teaches Adventure Photography",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Jodie Foster Teaches Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Joyce Carol Oates Teaches the Art of the Short Story",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Judd Apatow Teaches Comedy",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Judy Blume Teaches Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Kelly Wearstler Teaches Interior Design",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Ken Burns Teaches Documentary Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Lynnette Marrero & Ryan Chetiyawardana Teach Mixology",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Malcolm Gladwell Teaches Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Marc Jacobs Teaches Fashion Design",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Margaret Atwood Teaches Creative Writing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Martin Scorsese Teaches Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Massimo Bottura Teaches Modern Italian Cooking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Mira Nair Teaches Independent Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Misty Copeland Teaches Ballet Technique and Artistry",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Natalie Portman Teaches Acting",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Neil Gaiman Teaches the Art of Storytelling",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Neil deGrasse Tyson Teaches Scientific Thinking and Communication",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Parris Goebel Teaches Creativity in Choreography",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Paul Krugman Teaches Economics and Society",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Penn & Teller Teach the Art of Magic",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Phil Ivey Teaches Poker Strategy",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "R.L. Stine Teaches Writing for Young Audiences",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Reba McEntire Teaches Country Music",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Robin Roberts Teaches Effective and Authentic Communication",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Ron Finley Teaches Gardening",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Ron Howard Teaches Directing",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "RuPaul Teaches Self-Expression and Authenticity",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Samuel L. Jackson Teaches Acting",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Sara Blakely Teaches Self-Made Entrepreneurship",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Serena Williams Teaches Tennis",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Sheila E. Teaches Drumming and Percussion",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Shonda Rhimes Teaches Writing for Television",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Simone Biles Teaches Gymnastics Fundamentals",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Spike Lee Teaches Independent Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "St. Vincent Teaches Creativity and Songwriting",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Stephen Curry Teaches Shooting, Ball-Handling, and Scoring",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Steve Martin Teaches Comedy",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Tan France Teaches Style for Everyone",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Thomas Keller Teaches Cooking Techniques I Vegetables, Pasta, and Eggs",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Thomas Keller Teaches Cooking Techniques II Meats, Stocks, and Sauces",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Thomas Keller Teaches Cooking Techniques III Seafood, Sous Vide, and Desserts",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Timbaland Teaches Producing and Beatmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Tom Morello Teaches Electric Guitar",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Tony Hawk Teaches Skateboarding",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Usher Teaches The Art of Performance",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Walter Mosley Teaches Fiction and Storytelling",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Werner Herzog Teaches Filmmaking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Will Wright Teaches Game Design and Theory",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Wolfgang Puck Teaches Cooking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
+  {
+    "title": "Yotam Ottolenghi Teaches Modern Middle Eastern Cooking",
+    "links": {
+      "main": "",
+      "mirrors": []
+    },
+    "tags": []
+  },
 ]
